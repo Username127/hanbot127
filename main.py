a4cd936c4d0777ca5b01867413c273f0c4f6e3f1
Username127 <59255815+Username127@users.noreply.github.com>
2019-12-27 16:33:17 +0600

    Update main.py

--- main.py
+++ main.py
@@ -168,5 +168,5 @@ def index():
      return "Hello from Heroku!", 200
      
 
-if _name_ == "_main_":
+if __name__ == "__main__":
      server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
